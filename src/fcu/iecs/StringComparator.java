1de8e5544053b99c1933c14b91ae815e82c46788
ASUS <ASUS@ASUS-NB>
2016-10-18 22:26:46 +0800

    HW2-1

--- src/fcu/iecs/StringComparator.java
+++ src/fcu/iecs/StringComparator.java
@@ -6,21 +6,22 @@ public class StringComparator {
 
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		String str1,str2;
-		System.out.println("Enter a string 1¡G");
-		str1=scanner.next();
-		System.out.println("Enter a string 2¡G");
-		str2=scanner.next();
-		
 		int a;
-		a=str1.compareToIgnoreCase(str2);
-		if(a==0){
-			System.out.println("The two strings are the same");
-		}
-		else{
-			System.out.println("The two strings are not the same");
-		}
+		do{
+			String str1,str2;
+			System.out.println("Enter a string 1¡G");
+			str1=scanner.next();
+			System.out.println("Enter a string 2¡G");
+			str2=scanner.next();
+			
+			a=str1.compareToIgnoreCase(str2);
+			if(a==0){
+				System.out.println("The two strings are the same");
+			}
+			else{
+				System.out.println("The two strings are not the same");
+			}
+		}while(a!=0);
 	}
 
 }
